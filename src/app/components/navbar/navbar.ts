7f7e55ac5f185b31d9fc7eaf415de5246b90b4ee
LordElias <kerlinelias@gmail.com>
2026-03-27 23:21:46 +0100

    feat: implement initial settings design

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,16 +1,23 @@
-import { Component, inject } from '@angular/core';
-import { Auth } from '@services/auth'
+import { Component, ChangeDetectionStrategy, inject, output } from '@angular/core';
+import { Auth } from '@services/auth';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-  private authService: Auth = inject(Auth);
+  private authService = inject(Auth);
+
+  settingsRequested = output<void>();
 
   logout() {
     this.authService.logout();
+  }
+
+  openSettings() {
+    this.settingsRequested.emit();
   }
 }
